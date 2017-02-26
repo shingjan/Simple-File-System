997f7dcb4c22c838fe92f923cf7ae9c5e4293c1a
Isaac Shi <isaac@localhost.localdomain>
2017-02-26 15:49:48 +0800

    index changed

--- sfs.h
+++ sfs.h
@@ -1,9 +1,9 @@
 
-#define SB_OFFSET 512 /* The offset of superblock region*/
-#define INODE_OFFSET 4096 /* The offset of inode region */
-#define DATA_OFFSET 10485760 /* The offset of data region */
-#define MAX_INODE 100 /* The maximum number of inode */
-#define MAX_DATA_BLK 25600 /* The maximum number of block */
+#define SB_OFFSET 4096 /* The offset of superblock region*/
+#define INODE_OFFSET 8192 /* The offset of inode region */
+#define DATA_OFFSET 8388608 /* The offset of data region */
+#define MAX_INODE 170 /* The maximum number of inode */
+#define MAX_DATA_BLK 20480 /* The maximum number of block */
 #define BLOCK_SIZE 4096 /* The size per block */
 #define MAX_NESTING_DIR 10 /* The nesting number of directory */
 #define MAX_COMMAND_LENGTH 50 /* The maximum command length */

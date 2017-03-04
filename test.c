7beeef053cfe7c0001d748e69ff85d7bedf44b2d
ShingJan <yuanj.shi@hotmail.com>
2017-03-04 20:40:06 +0800

    changes on mac

--- test.c
+++ test.c
@@ -6,18 +6,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include "open2.h"
+#include "write_t.h"
+#include "read_t.h"
 
 int main(){
-	int fd = open("sht", O_WRONLY);
-	char buffer[4];
-	char buffer2[] = "l";
-	char x[10] = "sht1234567";
-	//if not such file for opening, file descriptor will return -1
-	int bytes = write(fd, buffer, 4*sizeof(char));
-	//printf("this is the fd: %d and bytes is: %d \n",fd,bytes);
-	//printf("%c \n",buffer[0]);
-	char* result;
-	strcpy(result,x);
-	printf("%d \n", strlen(result));
-	printf("%s \n", x);
-}				
+	int fd = open_t("sht", O_WRONLY);
+
+}
